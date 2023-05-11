f5a9b872bc70cf547d3b0981fb7084372e4ad662
Adrian Lacheta <adiczq@gmail.com>
2023-05-11 10:47:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
